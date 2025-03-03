a33e62f136236fe5993bd8b0c5a07b486237353f
Cory Zauss <cory.zauss@gmail.com>
2025-03-02 20:30:36 -0500

    identify function returns song response for valid songs

--- identify_song.py
+++ identify_song.py
@@ -1,5 +1,26 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# ! song is a file path
-def identify(song):
+API_KEY= os.getenv("AUDD_KEY")
+API_URL = "https://api.audd.io/"
+
+if not API_KEY:
+    raise ValueError("API key is missing! Check your .env file.")
+
+def identify(file):
+    with open(file, "rb") as song:
+        files = {"file": song} #attatch file
+        data = {"api_token": API_KEY}
+
+        response = requests.post(API_URL, files=files, data=data)
+
+        try:
+            response.raise_for_status() #raise error for bad http resp
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"ERROR in identify(): {e}")
+            return None
    
-    print(f"ID")
--- sort.py
+++ sort.py
@@ -8,31 +8,36 @@ from mutagen.id3 import ID3
 
 # look thru music folder to create plan:
 # aka a list of all the paths of all the files
-# check each file/ identify song
+# check each file/ identify song (AUDD ? )
 # rename and place in the correct artists folder with correct album and meta data 
 
 # path=input('enter path to music folder: ')
 
+#! temp paths ~ make user inmput path or select from GUI 
 root  = r"E:\\test_sort"
 output = r"E:\\test_sorted"
-extensions = ("mp3", "wav", "flac", "ogg", "aac", "m4a")
 
-                      
+extensions = ("mp3", "wav", "flac", "ogg", "aac", "m4a")
+        
 # Walk through directories manually
 for dirpath, _, filenames in os.walk(root):
     for filename in filenames:
         if filename.lower().endswith(extensions):  # Case insensitive check
-            file = os.path.join(dirpath, filename)
+            
+            #! here is the full file path of the audio file
+            file_path = os.path.join(dirpath, filename)
             
             try:
                 print(f"Current File: {filename}")
-                audio = MP3(file, ID3=ID3)
-        
+
+                audio = MP3(file_path, ID3=ID3)
                 artist = audio.get('TPE1', 'Unknown Artist')
                 title = audio.get('TPE2', 'Unknown Title')
                 print(artist, title)
-                # IF THINGS ARE UNKNOWN FIND FINGERPRINT
-            
+
+                #? IF THINGS ARE UNKNOWN FIND FINGERPRINT 
+                #? maybe just find fingerprint anyway
+                #?  If no print, can we guess thru the files name ?
             
             except Exception as e:
                 print(f"ERROR: {e}")
